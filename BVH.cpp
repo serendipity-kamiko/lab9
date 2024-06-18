b82b084150d7eae76d8f7989840df17f2295f4b1
serendipity-kamiko <165747398+serendipity-kamiko@users.noreply.github.com>
2024-06-18 15:25:45 +0800

    Update BVH.cpp

--- BVH.cpp
+++ BVH.cpp
@@ -106,4 +106,29 @@ Intersection BVHAccel::getIntersection(BVHBuildNode* node, const Ray& ray) const
 {
     // TODO Traverse the BVH to find intersection
 
+    std::array<int, 3> dirIsNeg; // 和getInsectin一样光线在各坐标轴方向的正负
+    dirIsNeg[0] = (ray.direction[0] > 0); //x
+    dirIsNeg[1] = (ray.direction[1] > 0); //y方向
+    dirIsNeg[2] = (ray.direction[2] > 0); //z方向
+
+    Intersection inter; //初始化一个表示相交信息的对象，初始默认为未找到交点
+
+    // 如果当前的节点其包围盒与光线没有交点，则直接返回
+    if (!node->bounds.IntersectP(ray, ray.direction_inv, dirIsNeg)) {
+        return inter;
+    }
+
+    // 如果当前节点是叶子节点
+    if (node->left == nullptr && node->right == nullptr) {
+        //直接看看这个节点的包围盒中的物体与光线的交点信息
+        return node->object->getIntersection(ray);
+    }
+
+    //左
+    Intersection leftInter = getIntersection(node->left, ray);
+    //右
+    Intersection rightInter = getIntersection(node->right, ray);
+
+    //比较左右子树返回的交点信息，返回距离光线源更近的那个交点信息
+    return leftInter.distance < rightInter.distance ? leftInter : rightInter;
 }
